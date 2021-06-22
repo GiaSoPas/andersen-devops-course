df1ea405cf44a1e7c8a22a660ede80237ca47819
nastsya fedotovna <gizar.zigangirov@gmail.com>
2021-06-22 19:39:23 +0300

    update bot

--- go_gitbot/bot.go
+++ go_gitbot/bot.go
@@ -2,17 +2,16 @@ package main
 
 import (
 	"log"
-
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
 )
 
+
 func main() {
-	bot, err := tgbotapi.NewBotAPI("1868882835:AAHBWO30JccYhZMBD2KqCmCFKV3UxU19ki4")
+	bot, err := tgbotapi.NewBotAPI(">>>>>TOKEN<<<<<")
 	if err != nil {
 		log.Panic(err)
 	}
-
-	bot.Debug = true
+	bot.Debug = false
 
 	log.Printf("Authorized on account %s", bot.Self.UserName)
 
@@ -31,8 +30,14 @@ func main() {
 		if update.Message.IsCommand() {
 			msg := tgbotapi.NewMessage(update.Message.Chat.ID, "")
 			switch update.Message.Command() {
+			case "start":
+				msg.Text = "Hi, i`m slave of nastasyafedotovna/andersen-devops-course repo.\nI can show you available commands by /commands command"
+			case "commands":
+				msg.Text = "Available commands:\n/start\n/commands\n/git\n/aboutme\n/tilblog\n/tasks\n/task1\n/task2\n/task3\n/task4\n/task5\n"
+			case "git":
+				msg.Text = "https://github.com/nastasyafedotovna/andersen-devops-course"
 			case "tasks":
-				msg.Text = "1. Ansible task\n2. One-liner to script task\n3. Git api script task\n4. Telegram bot on golang"
+				msg.Text = "TIL-blog and About me\n1. Ansible task\n2. One-liner to script task\n3. Git api script task\n4. Telegram bot on golang\n5. Docker task"
 			case "aboutme":
 				msg.Text = "https://github.com/nastasyafedotovna/andersen-devops-course/blob/main/aboutMyself/aboutme.md"
 			case "tilblog":
@@ -42,11 +47,13 @@ func main() {
 			case "task2":
 				msg.Text = "https://github.com/nastasyafedotovna/andersen-devops-course/tree/main/netstat_script"
 			case "task3":
-				msg.Text = "https://github.com/nastasyafedotovna/andersen-devops-course/tree/main/git_api"
-			case "task4":
 				msg.Text = "https://github.com/nastasyafedotovna/andersen-devops-course/tree/main/go_gitbot"
+			case "task4":
+				msg.Text = "https://github.com/nastasyafedotovna/andersen-devops-course/tree/main/git_api"
+			case "task5":
+				msg.Text = "https://github.com/nastasyafedotovna/andersen-devops-course/tree/main/docker_task"
 			default:
-				msg.Text = "I don't know that command"
+				msg.Text = "I don't know that command.\nYou can view available commands: /commands"
 			}
 			bot.Send(msg)
 		}
